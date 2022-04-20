499655aa54c6b77f7dc6066532f1bae0787d577b
Valentin Barbotin <valentin.barbotin@sixense-group.com>
2022-04-21 01:02:38 +0200

    feat(header): logo resize + avatar

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,17 +1,33 @@
 <header class="h-1/5">
     <div class="flex justify-between darkBackground p-4">
-        <img src="/img/logo4.png" alt="logo">
+        <a href="/">
+            <img width="250" height="250" src="/img/logo4.png" alt="logo">
+        </a>
         <div class="flex justify-end w-3/5 items-center text-white">
-
-            <ul class="flex">
-                <li class="hover:text-red-600 m-2">HOME</li>
+            
+            <ul class="flex gap-9 items-center">
+                <li class="hover:text-red-600 m-2">
+                    <a href="/">
+                        HOME
+                    </a>
+                </li>
                 <li class="hover:text-red-600 m-2">Application</li>
                 <li class="hover:text-red-600 m-2">About us</li>
                 <li>
-                    <button id="userDropdownButton" data-dropdown-toggle="userDropdown" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown header
-                        <svg class="ml-2 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
-                        </svg>
+                    <button data-dropdown-toggle="userDropdown" type="button">
+                        <span class="mdc-button__ripple"></span>
+                        <span class="mdc-button__touch"></span>
+                        <img class="w-16 h-16 rounded-full"
+                        {{ Auth::check() ? 'src=https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg?cs=srgb&dl=pexels-pixabay-60597.jpg&fm=jpg' : 'src=https://vistapointe.net/images/unknown-2.jpg' }}
+                        alt="Avatar">
+                        @if (Auth::check())
+                        <div class="relative">
+                            <span class="bottom-0 left-12 absolute w-3.5 h-3.5 bg-green-400 border-2 border-white dark:border-gray-800 rounded-full"></span>
+                        </div>
+                        @endif
+                        {{-- <svg class="ml-2 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+                        </svg> --}}
                     </button>
                 </li>
             </ul>
@@ -21,7 +37,11 @@
                 @auth
                 <div class="py-3 px-4 text-sm text-gray-900 dark:text-white">
                         <h2>{{ Auth::user()->name }}</h2>
-                        <div class="font-medium truncate">{{ Auth::user()->email }}</div>
+                        <div data-tooltip-target="tooltip-email" class="font-medium truncate">{{ Auth::user()->email }}</div>
+                        <div id="tooltip-email" role="tooltip" class="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700">
+                            {{ Auth::user()->email }}
+                            <div class="tooltip-arrow" data-popper-arrow></div>
+                        </div>
                 </div>
                 @endauth
                 <ul class="py-1 text-sm text-gray-700 dark:text-gray-200">
