fa334a7c389d090a46e9abd919c10d1cc40ee15f
Valentin Barbotin <valentin.barbotin@sixense-group.com>
2022-05-17 01:11:26 +0200

    feat: use attribute filters

--- resources/js/catalogue.ts
+++ resources/js/catalogue.ts
@@ -50,6 +50,57 @@ const addEVH = () => {
     });
 }
 
+const updateProducts = (activeAttributes: Set<number>) => {
+    const elems = document.querySelectorAll('tr[rowid]');
+
+    elems.forEach(elem => {
+        let matchAttributes = false;
+        const list = elem.querySelectorAll('td > div > ul')[0];
+
+        if (activeAttributes.size > 0) {
+            for (const child of Array.from(list.children)) {
+                const attr = child.getAttribute('attr');
+                if (attr && activeAttributes.has(parseInt(attr))) {
+                    matchAttributes = true;
+                }
+            }
+        } else {
+            matchAttributes = true;
+        }
+
+        matchAttributes ? elem.classList.remove('hidden') : elem.classList.add('hidden');
+    });
+}
+
+const filters = () => {
+    const attributes = document.querySelectorAll('input[type=checkbox]');
+
+    const attributesToShow = new Set<number>();
+    attributes.forEach(filter => {
+        filter.addEventListener('change', (event) => {
+            const target = event.target as HTMLInputElement;
+            if (!target) {
+                return;
+            }
+
+            const attributeId = target.getAttribute('attributeId');
+            if (!attributeId) {
+                return;
+            }
+
+            if (target.checked) {
+                attributesToShow.add(parseInt(attributeId));
+            } else {
+                attributesToShow.delete(parseInt(attributeId));
+            }
+
+            updateProducts(attributesToShow);
+        });
+    });
+}
+
 (async () => {
     addEVH();
+    filters();
+
 })();
--- resources/views/catalogue/catalogue.blade.php
+++ resources/views/catalogue/catalogue.blade.php
@@ -5,14 +5,14 @@
     
     <div class="m-2 w-11/12 mx-auto">
         @component('components.button', [
-            'text' => 'Voir la panier',
+            'text' => 'Voir le panier',
             'type' => 'button',
             'id' => 'cartCheckBtn',
         ])
         @endcomponent
     </div>
 
-    <div class="m-2 flex flex-col md:flex-row w-11/12 border mx-auto justify-between">
+    <div class="m-2 flex flex-col md:flex-row w-11/12 mx-auto justify-between">
         <div class="bg-white drop-shadow-md w-2/12 p-4">
             @component('components.inputfield', [
                 'text' => 'Search',
@@ -29,7 +29,7 @@
                 @foreach ($attributes as $attribute)
                 <div class="mdc-form-field">
                     <div class="mdc-checkbox">
-                        <input type="checkbox" class="mdc-checkbox__native-control" id="checkbox-{{ $attribute->id }}" />
+                        <input type="checkbox" class="mdc-checkbox__native-control" attributeId={{ $attribute->id }} id="checkbox-{{ $attribute->id }}" />
                         <div class="mdc-checkbox__background">
                             <svg class="mdc-checkbox__checkmark" viewBox="0 0 24 24">
                                 <path class="mdc-checkbox__checkmark-path" fill="none" d="M1.73,12.91 8.1,19.28 22.79,4.59" />
@@ -45,39 +45,49 @@
 
         </div>
 
-        <div class="border w-9/12">
+        <div class="w-9/12">
 
-            <div class="mdc-data-table">
+            <div class="mdc-data-table w-full">
                 <div class="mdc-data-table__table-container">
                   <table class="mdc-data-table__table">
                     <thead>
                       <tr class="mdc-data-table__header-row">
                         <th class="mdc-data-table__header-cell" role="columnheader" scope="col"></th>
-                        <th class="mdc-data-table__header-cell" role="columnheader" scope="col"></th>
+                        <th class="mdc-data-table__header-cell border w-2/6" role="columnheader" scope="col"></th>
                       </tr>
                     </thead>
                     <tbody id="catalogue" class="mdc-data-table__content">
                       @foreach ($products as $product)
-                      <tr productattributeParent class="mdc-data-table__row">
-                        <td class="mdc-data-table__cell flex">
-                            <img src="{{ asset('/img/koba.png') }}" alt="">
-                            <div class="ml-3 flex flex-col justify-around">
-                                <div>
-                                    <h2 class="text-lg ">{{ $product->name }}</h2>
-                                    <h2 class="text-slate-400">{{ $product->description }}</h2>
-                                    <h2 class="text-lg ">{{ $product->price }} €</h2>
-                                </div>
+                      <tr productattributeParent rowid="{{ $product->id }}" class="mdc-data-table__row">
+                        <td class="mdc-data-table__cell">
+                            <div class="p-3 flex">
+                              <img src="{{ asset('/img/koba.png') }}" alt="">
+                              <div class="ml-3 flex flex-col justify-around">
+                                  <div>
+                                      <h2 class="text-lg ">{{ $product->name }}</h2>
+                                      <h2 class="text-slate-400">{{ $product->description }}</h2>
+                                      <h2 class="text-lg ">{{ $product->price }} €</h2>
+                                  </div>
+                                  
 
-                                <div class="w-4/6">
-                                    @component('components.inputfield', [
-                                        'text' => 'Quantity',
-                                        'icon' => 'search',
-                                        'id' => 'productId-'.$product->id.'-quantity',
-                                        'name' => 'quantity',
-                                        'type' => 'number',
-                                        ])
+                                  <div class="w-4/6">
+                                      @component('components.inputfield', [
+                                          'text' => 'Quantity',
+                                          'icon' => 'search',
+                                          'id' => 'productId-'.$product->id.'-quantity',
+                                          'name' => 'quantity',
+                                          'type' => 'number',
+                                          ])
+                                      @endcomponent
+                                      @component('components.button', [
+                                        'text' => 'Add to cart',
+                                        'type' => 'button',
+                                        'id' => 'addToCartBtn-'.$product->id,
+                                        'customId' => $product->id,
+                                    ])
                                     @endcomponent
-                                </div>
+                                  </div>
+                              </div>
                             </div>
                         </td>
                         {{-- TODO: rework --}}
@@ -86,18 +96,10 @@
                                 <ul class="list-disc mb-5">
                                     @isset($attributesList[$product->id])
                                         @foreach ($attributesList[$product->id] as $attribute)
-                                        <li>{{ $attributes[$attribute]['name'] }}</li>
+                                        <li attr={{ $attributes[$attribute]['id'] }} class="text-lg">{{ $attributes[$attribute]['name'] }}</li>
                                         @endforeach
                                     @endisset
                                 </ul>
-    
-                                @component('components.button', [
-                                    'text' => 'Add to cart',
-                                    'type' => 'button',
-                                    'id' => 'addToCartBtn-'.$product->id,
-                                    'customId' => $product->id,
-                                ])
-                                @endcomponent
                             </div>
                         </td>
                       </tr>
