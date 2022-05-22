0425e1c508066d8d405a47deb29e68c66005c6da
Valentin Barbotin <valentin.barbotin@sixense-group.com>
2022-05-22 18:28:40 +0200

    refacto: group routes

--- routes/web.php
+++ routes/web.php
@@ -47,6 +47,7 @@ use App\Actions\User\SetUserRole;
 
 Route::get('/', [Index::class, 'index'])->name('index');
 Route::get('/logout', [Index::class, 'logout']);
+Route::get('/catalogue', [Catalogue::class, 'index'])->middleware('auth')->name('catalogue');
 
 Route::patch('/user/{id}/activation', SetUserActivation::class)->middleware('admin')->name('user.edit.activation');
 Route::patch('/user/{id}/role', SetUserRole::class)->middleware('admin')->name('user.edit.role');
@@ -57,54 +58,79 @@ Route::post('/forgot-password', [Authentication::class, 'forgotPasswordSubmit'])
 Route::get('/reset-password/{token}', [Authentication::class, 'resetPassword'])->middleware('guest')->name('password.reset');
 Route::post('/reset-password', [Authentication::class, 'resetPasswordSubmit'])->middleware('guest')->name('password.resetSubmit');
 
-Route::get('/catalogue', [Catalogue::class, 'index'])->middleware('auth')->name('catalogue');
-
-Route::get('/cart', [Panier::class, 'index'])->middleware('auth')->name('cart');
-Route::post('/cart/add', AddToCart::class)->middleware('auth')->name('cart.add');
-Route::post('/cart/set', SetCart::class)->middleware('auth')->name('cart.set');
-Route::post('/cart/payment', SingleCharge::class)->middleware('auth')->name('cart.payment');
-Route::get('/cart/total', GetCartTotal::class)->middleware('auth')->name('cart.total');
 
 Route::post('/pay/package', SingleCharge::class)->middleware('auth')->name('cart.payment');
 
 // Route::get('/user/paymentmethods', GetPaymentMethods::class)->middleware('auth')->name('user.payment.methods');
 
-Route::get('/dashboard', [Dashboard::class, 'index'])->middleware('auth')->name('dashboard');
-Route::get('/dashboard/stripe-portal', function (Request $request) {
-    return $request->user()->redirectToBillingPortal(route('index'));
-})->name('dashboard.stripe-portal');
-
-Route::get('/dashboard/orders', [Dashboard::class, 'orders'])->middleware('auth')->name('user.orders');
-
-Route::get('/dashboard/weather', [Dashboard::class, 'weather'])->middleware('auth')->name('dashboard_weather');
-
-Route::get('/dashboard/packages', [Dashboard::class, 'packages'])->middleware('auth')->name('user.packages');
-Route::post('/dashboard/admin/orders/details', GetOrdersDetails::class)->middleware("auth")->name('users.details');
-Route::get('/dashboard/admin/orders/{orderId}/content', GetOrderProducts::class)->middleware('auth')->name('user.orders.content');
-
-Route::get('/dashboard/admin/accounts', [Dashboard::class, 'accounts'])->middleware("admin")->name('admin.accounts');
-Route::get('/dashboard/admin/scooters', [Dashboard::class, 'scooter'])->middleware("admin")->name('admin.scooters');
-Route::get('/dashboard/admin/products', [Dashboard::class, 'products'])->middleware("admin")->name('admin.products');
-Route::get('/dashboard/admin/partnerships', [Dashboard::class, 'partnerships'])->middleware("admin")->name('admin.partnerships');
-Route::post('/dashboard/admin/partnerships', CreatePartnership::class)->middleware("admin")->name('admin.partnerships');
-Route::put('/dashboard/admin/partnerships/{id}', EditPartnership::class)->middleware("admin")->name('admin.partnerships.edit');
-Route::get('/dashboard/admin/partnerships/{id}/list', GetPartnershipProductsList::class)->middleware("admin")->name('admin.partnerships.list');
-Route::post('/dashboard/admin/partnership/details', GetPartnershipsDetails::class)->middleware("admin")->name('admin.products.details');
-
-Route::get('/weather/{day}', [Weather::class, 'list'])->middleware('admin')->name('weather');
 
 Route::post('/dashboard/packages/edit', EditUserPackage::class)->middleware('auth')->name('user.packages.edit');
 
+Route::group(['prefix' => 'cart'], function () {
+    Route::middleware(['auth'])->group(function () {
+        Route::get('/', [Panier::class, 'index'])->name('cart');
+        Route::post('/add', AddToCart::class)->name('cart.add');
+        Route::post('/set', SetCart::class)->name('cart.set');
+        Route::post('/payment', SingleCharge::class)->name('cart.payment');
+        Route::get('/total', GetCartTotal::class)->name('cart.total');
+    });
+});
 
-Route::post('/dashboard/admin/users/action', [Dashboard::class, 'action'])->middleware("admin")->name('admin.users.action');
-Route::post('/dashboard/admin/users/details', [Dashboard::class, 'details'])->middleware("admin")->name('admin.users.details');
 
-Route::post('/dashboard/admin/scooters/action', [ScooterController::class, 'action'])->middleware("admin")->name('admin.scooters.action');
-Route::post('/dashboard/admin/scooters/details', [ScooterController::class, 'details'])->middleware("admin")->name('admin.scooters.details');
+Route::controller(Dashboard::class)->group(function () {
 
-Route::post('/dashboard/admin/products', CreateProduct::class)->middleware("admin")->name('admin.products.create');
-Route::put('/dashboard/admin/products/{id}', EditProduct::class)->middleware("admin")->name('admin.products.edit');
-Route::post('/dashboard/admin/products/delete', DeleteProduct::class)->middleware("admin")->name('admin.products.delete');
-Route::post('/dashboard/admin/products/details', GetProductsDetails::class)->middleware("admin")->name('admin.products.details');
+    Route::get('/dashboard', 'index')->middleware('auth')->name('dashboard');
+    Route::get('/dashboard/stripe-portal', function (Request $request) {
+        return $request->user()->redirectToBillingPortal(route('index'));
+    })->name('dashboard.stripe-portal');
 
+    Route::group(['prefix' => 'dashboard/admin'], function () {
+        Route::middleware(['auth'])->group(function () {
+            Route::get('/orders', 'orders')->name('dashboard.orders');
+            Route::get('/packages', 'packages')->name('dashboard.packages');
+            Route::get('/weather', 'weather')->name('dashboard.weather');
+            Route::get('/packages', 'packages')->name('dashboard.packages');
+        });
+    });
+
+    Route::group(['prefix' => 'dashboard/admin'], function () {
+        Route::middleware(['admin'])->group(function () {
+            Route::get('/accounts', 'accounts')->name('admin.accounts');
+            
+            Route::group(['prefix' => 'scooters'], function () {
+                Route::get('/', 'scooter')->name('admin.scooters');
+                Route::post('/action', [ScooterController::class, 'action'])->name('admin.scooters.action');
+                Route::post('/details', [ScooterController::class, 'details'])->name('admin.scooters.details');
+            });
+
+            Route::group(['prefix' => 'products'], function () {
+                Route::get('/', 'products')->name('admin.products');
+                Route::post('/details', GetProductsDetails::class)->name('admin.products.details');
+                Route::post('/', CreateProduct::class)->middleware("admin")->name('admin.products.create');
+                Route::put('/{id}', EditProduct::class)->middleware("admin")->name('admin.products.edit');
+                Route::post('/delete', DeleteProduct::class)->middleware("admin")->name('admin.products.delete');
+            });
+
+            Route::group(['prefix' => 'partnerships'], function () {
+                Route::get('/', 'partnerships')->name('admin.partnerships');
+                Route::post('/', CreatePartnership::class)->name('admin.partnerships');
+                Route::put('/{id}', EditPartnership::class)->name('admin.partnerships.edit');
+                Route::get('/{id}/list', GetPartnershipProductsList::class)->name('admin.partnerships.list');
+                Route::post('/details', GetPartnershipsDetails::class)->name('admin.products.details');
+            });
+
+            Route::group(['prefix' => 'orders'], function () { // TODO: check
+                Route::post('/details', GetOrdersDetails::class)->name('users.details');
+                Route::get('/{orderId}/content', GetOrderProducts::class)->name('user.orders.content');
+            });
+
+            Route::group(['prefix' => 'users'], function () {
+                Route::post('/action', 'action')->name('admin.users.action');
+                Route::post('/details', 'details')->name('admin.users.details');
+            });
+        });
+    });
+});
+
+Route::get('/weather/{day}', [Weather::class, 'list'])->middleware('admin')->name('weather');
 Route::get('/getwaypoints', GetWaypoints::class)->middleware('auth')->name('getwaypoints');
