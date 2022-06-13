9c35e33fad2cdbfc66975d2307b1b8a6929632b6
Valentin Barbotin <valentin.barbotin@sixense-group.com>
2022-06-13 14:23:23 +0200

    feat: maps canvas

---
+++ resources/views/admin/maps.blade.php
@@ -0,0 +1,49 @@
+<div class="relative overflow-x-auto lg:w-full mx-10 my-10">
+    <div class="w-full h-96" id="map">
+    </div>
+    {{-- <div class="gap-3 lg-5/6 md:w-full p-4 m-5 mx-auto flex items-center bg-white drop-shadow-md"> --}}
+        {{-- @component('components.inputfield', [
+            'text' => 'Search',
+            'icon' => 'search',
+            'name' => 'search',
+            'type' => 'search',
+            'id' => 'searchField',
+        ])
+        @endcomponent
+        @component('components.button', [
+            'text' => 'View details',
+            'type' => 'button',
+            'id' => 'viewDetailsBtn',
+            'modal' => 'modal-details',
+        ])
+        @endcomponent
+        @component('components.button', [
+            'text' => 'Delete',
+            'type' => 'button',
+            'id' => 'deleteBtn',
+        ])
+        @endcomponent
+        @component('components.button', [
+            'text' => 'Create',
+            'type' => 'button',
+            'id' => 'createBtn',
+            'modal' => 'modal-creation',
+        ])
+        @endcomponent
+        @component('components.button', [
+            'text' => 'Edit',
+            'type' => 'button',
+            'id' => 'editBtn',
+            'modal' => 'modal-edit',
+        ])
+        @endcomponent --}}
+    {{-- </div> --}}
+</div>
+
+@csrf
+<script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+<script src="/js/admin.maps.js"></script>
+
+{{-- <script async
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyATtyjZ7k1VD5NLIaJ-pKjcJ-QFoic_nh4&callback=initMap">
+</script> --}}
