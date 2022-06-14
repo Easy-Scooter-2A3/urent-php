31c7f6ef78d0dc8aee8c9c03bf8a745e8bec66e3
Valentin Barbotin <valentin.barbotin@sixense-group.com>
2022-06-14 22:49:00 +0200

    feat(maintenance): migration

---
+++ database/migrations/2022_06_13_214803_maintenance.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('maintenance', function (Blueprint $table) {
+            $table->id('id');
+            $table->foreignId('scooter_id')->constrained('scooters');
+            $table->string('description');
+            $table->string('notes')->nullable();
+            $table->timestamp('created_at');
+            $table->timestamp('finished_at')->nullable();
+            $table->foreignId('agent_id')->nullable()->constrained('users');
+        });
+
+        Schema::table('scooters', function (Blueprint $table) {
+            if (Schema::hasColumn('scooters', 'status')) {
+                $table->dropColumn('status');
+            } else {
+                $table->integer('status')->default(0);
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('maintenance');
+        Schema::table('scooters', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+    }
+};
