1e276b75fba016f44d6c625cb2250a00a7b4c1d8
Kyliann <kyliann.bourdonnay@sixense-group.com>
2022-06-15 16:12:42 +0200

    feat : footer translation

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -3,13 +3,13 @@
         <hr>
         <ul class="flex w-3/6 ml-36">
             <li class="hover:text-red-600 m-2">Copyright 2022 URent</li>
-            <li class="hover:text-red-600 m-2">Mentions légales</li>
-            <li class="hover:text-red-600 m-2">Confidentialité des données</li>
+            <li class="hover:text-red-600 m-2">@lang('Legal notice')</li>
+            <li class="hover:text-red-600 m-2">@lang('Data privacy')</li>
         </ul>
 
         <ul class="flex justify-end w-3/6">
-            <li class="hover:text-red-600 m-2">Suivez nous</li>
-            <li class="hover:text-red-600 m-2">Reseaux</li>
+            <li class="hover:text-red-600 m-2">@lang('Follow us')</li>
+            <li class="hover:text-red-600 m-2">@lang('Networks')</li>
         </ul>
     </div>
 </footer>
