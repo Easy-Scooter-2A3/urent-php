fe7b4ac00e9522e0d22ed6274f797532d6d71eab
msterhuj <gabin.lanore@gmail.com>
2022-06-21 19:51:16 +0200

    feat: 3xc message support

--- resources/views/head.blade.php
+++ resources/views/head.blade.php
@@ -30,5 +30,61 @@
         });
     </script>
 
+<call-us 
+     style="position: fixed; right: 20px; bottom: 20px; 
+         font-family: Arial; 
+         z-index: 99999; 
+         --call-us-form-header-background:#373737;
+         --call-us-main-button-background:#fe0018;
+         --call-us-client-text-color:#d4d4d4;
+         --call-us-agent-text-color:#eeeeee;
+         --call-us-form-height:330px;" 
+     id="wp-live-chat-by-3CX" 
+     channel-url="https://urent.on3cx.fr" 
+     files-url="https://urent.on3cx.fr" 
+     minimized="true" 
+     animation-style="none" 
+     party="gabinlanore" 
+     minimized-style="BubbleRight" 
+     allow-call="false" 
+     allow-video="false" 
+     allow-soundnotifications="true" 
+     enable-mute="true" 
+     enable-onmobile="true" 
+     offline-enabled="true" 
+     enable="true" 
+     ignore-queueownership="false" 
+     authentication="name" 
+     operator-name="Support" 
+     show-operator-actual-name="true" 
+     channel="phone" 
+     aknowledge-received="true" 
+     gdpr-enabled="false" 
+     gdpr-message="I agree that my personal data to be processed and for the use of cookies in order to engage in a chat processed by COMPANY, for the purpose of Chat/Support for the time of  30 day(s) as per the GDPR." 
+     message-userinfo-format="both" 
+     message-dateformat="both" 
+     start-chat-button-text="Chat" 
+     window-title="Live Chat & Talk" 
+     button-icon-type="Default" 
+     invite-message="Hello! How can we help you today?" 
+     authentication-message="Could we have your name and email?" 
+     unavailable-message="We are away, leave us a message!" 
+     offline-finish-message="We received your message and we'll contact you soon." 
+     ending-message="Your session is over. Please feel free to contact us again!" 
+     greeting-visibility="none" 
+     greeting-offline-visibility="none" 
+     chat-delay="2000" 
+     offline-name-message="Could we have your name?" 
+     offline-email-message="Could we have your email?" 
+     offline-form-invalid-name="I'm sorry, the provided name is not valid." 
+     offline-form-maximum-characters-reached="Maximum characters reached" 
+     offline-form-invalid-email="I'm sorry, that doesn't look like an email address. Can you try again?" 
+     enable-direct-call="true" 
+     enable-ga="false" 
+     >
+</call-us>
+
+<script defer src="https://cdn.3cx.com/livechat/v1/callus.js" id="tcx-callus-js"></script>
+
     <title>Document</title>
 </head>
