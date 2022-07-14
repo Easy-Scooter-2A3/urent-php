29220c66ce33600f01bc4a39c09fc9cc5db54b9b
Valentin Barbotin <valentin.barbotin@sixense-group.com>
2022-07-14 21:23:10 +0200

    fix: typo

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -115,7 +115,7 @@
 
         @auth
           <li>
-            <a href="{{ route('logout') }}" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-red-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">{{ __('Sing out') }}</a>
+            <a href="{{ route('logout') }}" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-red-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">{{ __('Sign out') }}</a>
           </li>
         @else
           <li>
